489f3c5c9fadb07addf24c5fba93aaeea23548de
Vivek Singh <vivek.singh57@ymail.com>
2023-10-29 15:02:28 +0530

    Initial commite done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import React, {Fragment, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
 
 function App() {
+  const [inputItem, setInput] = useState("");
+  const [listItems, setList] = useState([]);
+  // const [isDone, setDone] = useState(false);
+
+  const setItemList = (e) => {
+    setInput(e.target.value);
+  };
+
+  const addItemList = () => {
+    setList((previous_values) => {
+      return [...previous_values, inputItem];
+    });
+    setInput('');
+  };
+
+  const removeItem = (key) => {
+    setList((previous_values)=>{
+      return previous_values.filter((e, i)=>{
+        return i !== key;
+      });
+    });
+    // setDone(true);
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to react project
-        </a>
-      </header>
-    </div>
+
+    <Fragment>
+      <div className="app">
+         <h1 className='heading'>My ToDo List</h1>
+           <input type="text"
+             className="input_form" 
+             name="todo" 
+             placeholder="Add ToDo Here.."
+             onChange={setItemList}
+             value={inputItem}
+             />
+             <button type="button" className="todoBtn" onClick={addItemList}>+</button>
+
+            {
+            listItems.map((list, index)=>{
+              return <div className='todoList' key={index}>
+              <ol className="list-group list-group-flush">
+                <li className="list-group-item d-flex justify-content-between align-items-start list-group-item-action">
+                  <div className="ms-2 me-auto">
+                    <div className="fw-bold text_wrap">{list}</div>
+                  </div>
+                  <span className="badge bg-danger bg-gradient rounded-pill removeBtn" onClick={()=>removeItem(index)}>Remove</span>
+                </li>
+              </ol>
+            </div>
+            })
+            }
+      </div>
+    </Fragment>
   );
 }
 
